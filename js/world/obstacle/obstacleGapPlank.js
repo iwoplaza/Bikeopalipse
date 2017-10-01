012481c8d82a3c155b267719a8179a06a5d241a0
Iwo Plaza <gobbobpl@gmail.com>
2017-10-01 20:32:44 +0200

    Updated the meteor.

--- js/world/obstacle/obstacleGapPlank.js
+++ js/world/obstacle/obstacleGapPlank.js
@@ -2,7 +2,7 @@ function ObstacleGapPlank() {
     ObstacleBase.call(this);
     this.location.y = -World.roadHeight;
     this.gapWidth = 60;
-    this.plankWidth = 21;
+    this.plankWidth = 24;
     this.plankY = Math.floor(Math.random()*(World.roadHeight-this.plankWidth));
     this.collisionBoundsTop = new Bounds(10, 0, this.gapWidth-10, this.plankY);
     this.collisionBoundsBottom = new Bounds(10, this.plankY+this.plankWidth, this.gapWidth-10, World.roadHeight);
@@ -14,7 +14,7 @@ ObstacleGapPlank.prototype.draw = function() {
     ctx.translate(Math.floor(this.location.x), this.location.y);
 	
 	ctx.drawImage(Obstacle.image, 0, 0, 77, 105, 0, 0, 77, 107);
-	ctx.drawImage(Obstacle.image, 0, 118, 82, 19, 0, this.plankY+2, 82, 19);
+	ctx.drawImage(Obstacle.image, 0, 118, 82, 19, 0, this.plankY+3, 82, 19);
 	
     ctx.restore();
 }
